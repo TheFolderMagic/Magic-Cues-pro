3ea5d1ad3463422b0e445fb5dc47b0484895f657
TheFolderMagic <origamiboy8@gmail.com>
2026-06-11 14:02:47 +0200

    MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,9 +1,13 @@
 package __PACKAGE__;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.webkit.WebView;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
@@ -12,6 +16,11 @@ public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        // Request native system microphone permission on startup if not already granted
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, 101);
+        }
         
         try {
             WebView webView = MainActivity.this.getBridge().getWebView();
