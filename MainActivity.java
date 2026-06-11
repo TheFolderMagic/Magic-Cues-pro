568b3a8950070c8e0b495f6b1a6152ef083c7eef
TheFolderMagic <origamiboy8@gmail.com>
2026-06-11 12:41:16 +0200

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,56 @@
+package __PACKAGE__;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {
+    private NfcBridge nfcBridge = null;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        // Defer NFC setup so Capacitor has time to fully initialize
+        new Handler(Looper.getMainLooper()).post(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    if (bridge != null && bridge.getWebView() != null) {
+                        nfcBridge = new NfcBridge(MainActivity.this, bridge.getWebView());
+                        bridge.getWebView().addJavascriptInterface(nfcBridge, "Android");
+                    }
+                } catch (Exception e) {
+                    Log.e("MagicCues", "NFC setup failed: " + e.getMessage());
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (nfcBridge != null) {
+            nfcBridge.enableForegroundDispatch();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (nfcBridge != null) {
+            nfcBridge.disableForegroundDispatch();
+        }
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        if (nfcBridge != null) {
+            nfcBridge.handleIntent(intent);
+        }
+    }
+}
